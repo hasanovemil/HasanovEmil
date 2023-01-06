53f18ac24ae362677f0175f188e049207d179ef3
hasanovemil <emil-2035@mail.ru>
2023-01-06 23:14:43 +0400

    hasanov emil123

---
+++ src/day47_Encapsulation/CapitalOneEmployees.java
@@ -0,0 +1,56 @@
+package day47_Encapsulation;
+
+public class CapitalOneEmployees {
+
+    public String employeename;
+    public int age;
+    public String jobtitle;
+
+
+
+
+    public CapitalOneEmployees(String employeename,int age,String jobtitle){
+        this.employeename = employeename;
+        this.age = age;
+        this.jobtitle = jobtitle;
+    }
+
+    public static String companyname;
+
+    static{
+        companyname = "capital one";
+    }
+
+    private int ID;
+    private double salary;
+    private String address;
+
+
+    public void setID(int ID){
+        this.ID = ID;
+    }
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public int getID(){
+        return ID;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public String getAddress(){
+        return address;
+    }
+
+
+
+
+
+
+}
+
+

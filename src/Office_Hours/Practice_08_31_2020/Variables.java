94f54660caeeb189dd693cea821ce39a397709eb
hasanovemil <emil-2035@mail.ru>
2023-01-07 22:26:00 +0400

    hasanov emil123

---
+++ src/Office_Hours/Practice_08_31_2020/Variables.java
@@ -0,0 +1,14 @@
+package Office_Hours.Practice_08_31_2020;
+
+public class Variables {
+     int z = 400;
+     static int y = 600;
+
+
+    public static void main(String[] args) {
+        int a = 100;
+        System.out.println(a);
+
+
+    }
+}

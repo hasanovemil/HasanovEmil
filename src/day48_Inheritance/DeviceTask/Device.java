172bd4c996067090a9e98ea3cc580d09b0de665c
hasanovemil <emil-2035@mail.ru>
2023-01-07 17:02:13 +0400

    hasanov emil123

---
+++ src/day48_Inheritance/DeviceTask/Device.java
@@ -0,0 +1,35 @@
+package day48_Inheritance.DeviceTask;
+
+public class Device {
+
+    public String brand;
+    public String model;
+    public double price;
+    public boolean hasbattery;
+    public boolean hasmemory;
+    public String screensize;
+
+    public static String madein;
+    public static boolean isElectronic;
+
+    static {
+        madein = "China";
+        isElectronic = true;
+    }
+
+    public void setInfo(String brand, String model, double price, boolean hasbattery, boolean hasmemory, String screensize) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.hasbattery = hasbattery;
+        this.hasmemory = hasmemory;
+        this.screensize = screensize;
+    }
+
+
+
+
+}
+
+
+

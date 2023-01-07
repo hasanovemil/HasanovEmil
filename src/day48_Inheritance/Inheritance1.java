ecc3c8a43cca0253082b7eb9392bcda42af0cd3c
hasanovemil <emil-2035@mail.ru>
2023-01-07 16:38:36 +0400

    hasanov emil123

---
+++ src/day48_Inheritance/Inheritance1.java
@@ -0,0 +1,15 @@
+package day48_Inheritance;
+
+class A{
+      static int a = 10;
+}
+class B extends A{
+   static int b = 20;
+}
+class C extends B{
+     static int c = 30;
+}
+
+public class Inheritance1 {
+
+}

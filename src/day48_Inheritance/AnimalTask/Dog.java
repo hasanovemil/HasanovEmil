d596e4b21d4dd5cdf37edc4f27fce9e0cfb496cf
hasanovemil <emil-2035@mail.ru>
2023-01-07 15:11:51 +0400

    hasanov emil123

--- src/day48_Inheritance/AnimalTask/Dog.java
+++ src/day48_Inheritance/AnimalTask/Dog.java
@@ -2,7 +2,16 @@ package day48_Inheritance.AnimalTask;
 
 public class Dog extends Animal {
 
+
+    public Dog(String name,String size,int age,char gender){
+          setInfo(name,size,age,gender);
+    }
+
+
+
         public void bark(){
-            System.out.println(name+"is barking");
+            System.out.println(name+" is barking");
         }
+
+
 }

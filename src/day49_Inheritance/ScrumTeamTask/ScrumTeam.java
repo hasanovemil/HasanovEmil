0d567a9817920b597242e970814c1a604c472142
hasanovemil <emil-2035@mail.ru>
2023-01-08 20:38:51 +0400

    hasanov emil123

--- src/day49_Inheritance/ScrumTeamTask/ScrumTeam.java
+++ src/day49_Inheritance/ScrumTeamTask/ScrumTeam.java
@@ -1,6 +1,7 @@
 package day49_Inheritance.ScrumTeamTask;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ScrumTeam {
 
@@ -13,5 +14,13 @@ public class ScrumTeam {
 
     public void addDevelopers(Developer developer){
         developers.add(developer);
+    }
+
+    public void addTesters(Tester[] testers){
+        this.testers.addAll(Arrays.asList(testers));
+    }
+
+    public void addDevelopers(Developer[] developers){
+        this.developers.addAll(Arrays.asList(developers));
     }
 }

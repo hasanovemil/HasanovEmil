ae158853bac8150d6e2e5eef7caee236bcc3dc57
hasanovemil <emil-2035@mail.ru>
2023-01-09 23:57:30 +0400

    hasanov emil123

---
+++ src/day50_Inheritance_Overriding/ShapeTask/Shape.java
@@ -0,0 +1,20 @@
+package day50_Inheritance_Overriding.ShapeTask;
+
+public class Shape {
+
+    public double area;
+    public double perimeter;
+
+
+
+    public double calculateArea(){
+        return 0;
+    }
+    public double calculatePerimeter(){
+        return 0;
+    }
+
+
+
+
+}

6f269caf6602cf452a0f034c89c8159875ea3a0e
hasanovemil <emil-2035@mail.ru>
2023-01-09 21:35:53 +0400

    hasanov emil123

--- src/day50_Inheritance_Overriding/Test.java
+++ src/day50_Inheritance_Overriding/Test.java
@@ -1,6 +1,13 @@
 package day50_Inheritance_Overriding;
 
-public class Test {
+public class Test extends AccessModifiers {
 
+    public static void main(String[] args) {
 
+        System.out.println(Test.publicData);
+        System.out.println(Test.protectedData);
+        System.out.println(Test.defaultdata);
+        // System.out.println(Test.privatedata);
+
+    }
 }

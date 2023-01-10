7b15fdafaba55ba41ac328c41c686d0d6b911869
hasanovemil <emil-2035@mail.ru>
2023-01-10 16:12:15 +0400

    hasanov emil123

---
+++ src/day51_Exceptions/methodOverriding.java
@@ -0,0 +1,21 @@
+package day51_Exceptions;
+
+class A {
+    public void method(){
+
+    }
+}
+
+public class methodOverriding extends A {
+    @Override
+    public void method(){
+
+    }
+
+
+    @Override
+    public String toString(){
+        return "example";
+    }
+
+}

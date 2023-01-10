e6c20e9adbd3826981dca47f2d46948937b6398a
hasanovemil <emil-2035@mail.ru>
2023-01-10 21:45:30 +0400

    hasanov emil123

---
+++ src/day51_Exceptions/PetsTask/Pet.java
@@ -0,0 +1,21 @@
+package day51_Exceptions.PetsTask;
+
+public class Pet {
+
+    public String name;
+    public int age;
+    public char gender;
+    public String breed;
+    public String color;
+
+    public Pet(String name, int age, char gender, String breed, String color) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.breed = breed;
+        this.color = color;
+    }
+
+
+
+}

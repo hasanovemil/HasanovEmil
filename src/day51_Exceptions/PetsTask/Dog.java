74cef776a0396c7aebd413703bba3c19b0a603b6
hasanovemil <emil-2035@mail.ru>
2023-01-10 23:03:51 +0400

    hasanov emil123

---
+++ src/day51_Exceptions/PetsTask/Dog.java
@@ -0,0 +1,22 @@
+package day51_Exceptions.PetsTask;
+
+public class Dog extends Pet   {
+
+
+       public Dog(String name, int age, char gender, String breed, String color) {
+              super(name, age, gender, breed, color);
+       }
+
+       @Override
+       public String toString() {
+              return "Dog{" +
+                      "name='" + name + '\'' +
+                      ", age=" + age +
+                      ", gender=" + gender +
+                      ", breed='" + breed + '\'' +
+                      ", color='" + color + '\'' +
+                      '}';
+       }
+}
+
+
---
+++ src/day51_Exceptions/PetsTask/House.java
@@ -0,0 +1,12 @@
+package day51_Exceptions.PetsTask;
+
+public class House {
+
+    public static void main(String[] args) {
+
+         Dog dog1 = new Dog("Winton",4,'M',"Scotty Terrier","White");
+
+        System.out.println(dog1);
+
+    }
+}

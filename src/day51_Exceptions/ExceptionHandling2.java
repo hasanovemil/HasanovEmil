c576249f0274bf893eca6efc22a9cf4545279561
hasanovemil <emil-2035@mail.ru>
2023-01-11 23:49:08 +0400

    hasanov emil123

--- src/day51_Exceptions/ExceptionHandling2.java
+++ src/day51_Exceptions/ExceptionHandling2.java
@@ -2,5 +2,16 @@ package day51_Exceptions;
 
 public class ExceptionHandling2 {
 
+    public static void main(String[] args) {
+        String exceptionResult = "";
+       try{
+           System.out.println(10/0);
+       } catch(ArithmeticException e){
+           exceptionResult = e.getMessage();
+           System.out.println("Arithmetic exception is detected");
+       }
 
+        System.out.println(exceptionResult);
+
+    }
 }

6743be30788fd312e70ebf48be939a9d6c1c69c0
hasanovemil <emil-2035@mail.ru>
2023-01-11 23:03:56 +0400

    hasanov emil123

---
+++ src/day51_Exceptions/ExceptionsIntro.java
@@ -0,0 +1,13 @@
+package day51_Exceptions;
+
+public class ExceptionsIntro {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+
+        System.out.println(arr[100]);
+        // Thread.sleep(3000);
+
+
+    }
+}

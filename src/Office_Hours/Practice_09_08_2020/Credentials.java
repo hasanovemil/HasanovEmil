18a969a1e3ca9e17024712596598d90fd8073b31
hasanovemil <emil-2035@mail.ru>
2023-01-13 01:07:04 +0400

    hasanov emil123

--- src/Office_Hours/Practice_09_08_2020/Credentials.java
+++ src/Office_Hours/Practice_09_08_2020/Credentials.java
@@ -5,5 +5,19 @@ public class Credentials {
     private String username = "Cybertek";
     private String password = "Cybertek1234";
 
+     public String getUsername(){
+        return username;
+    }
 
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/Office_Hours/Practice_09_08_2020/TestCases.java
+++ src/Office_Hours/Practice_09_08_2020/TestCases.java
@@ -2,5 +2,44 @@ package Office_Hours.Practice_09_08_2020;
 
 public class TestCases {
 
+    public static void main(String[] args) {
+
+        Credentials c1 = new Credentials();
+
+        //System.out.println(c1.username);
+        //System.out.println(c1.password);
+
+        System.out.println("User name entered: " + c1.getUsername());
+
+       // c1.Username = "Batch20";
+
+        c1.setUsername("Emil20");
+
+        System.out.println(c1.getUsername());
+
+        c1.setPassword("Rashad91");
+
+        System.out.println(c1.getPassword());
+
+
+
+
+
+    }
+
+    /*public String getUsername (){
+        return username;
+    }
+    */
+
+
+
+
+
+
+
+
+
+
 
 }

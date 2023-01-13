af1da2ab5c8ab94f4faf40bec54ad2b30de0797d
hasanovemil <emil-2035@mail.ru>
2023-01-13 22:06:59 +0400

    hasanov emil123

--- src/day52_Exceptions/MultiCatchBlock.java
+++ src/day52_Exceptions/MultiCatchBlock.java
@@ -9,8 +9,34 @@ public class MultiCatchBlock {
         try{
             System.out.println(arr[20]);
         }catch(ArithmeticException e){
-            System.out.println("");
+            System.out.println("Aritmetic exception is handled");
+        }catch(ClassCastException e){
+            System.out.println("ClassCastException");
+        }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Array Index Out Of Bounds Exception");
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Index out of bounds exception");
+        }catch (RuntimeException e){
+            System.out.println("Run time Exception");
         }
+
+        System.out.println("===============================================");
+
+        String str = "Hasanov";
+        try {
+            System.out.println(str.charAt(200));
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("==========================================");
+
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+
+        }
+        System.out.println("Completed");
+
 
     }
 }

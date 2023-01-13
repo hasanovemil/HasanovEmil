61aec1135dfc2ef1dd6f94af2b306c06f8b76653
hasanovemil <emil-2035@mail.ru>
2023-01-13 18:21:29 +0400

    hasanov emil123

---
+++ src/day52_Exceptions/MultiCatchBlock.java
@@ -0,0 +1,15 @@
+package day52_Exceptions;
+
+public class MultiCatchBlock {
+
+    public static void main(String[] args) {
+        int[] arr = { 10 ,20 };
+
+        try{
+            System.out.println(arr[20]);
+        }catch(ArithmeticException e){
+            System.out.println("");
+        }
+
+    }
+}
---
+++ src/day52_Exceptions/main.java
@@ -0,0 +1,14 @@
+package day52_Exceptions;
+
+public class main {
+
+    public static void main(String[] args) {
+        try{
+            System.out.println(9/0);
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Test completed");
+
+    }
+}

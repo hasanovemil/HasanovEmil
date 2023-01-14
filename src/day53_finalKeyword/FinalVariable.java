281d5d5663825812b8b76dd52ee1bf8b6d592cc0
hasanovemil <emil-2035@mail.ru>
2023-01-14 20:38:54 +0400

    hasanov emil123

--- src/day53_finalKeyword/FinalVariable.java
+++ src/day53_finalKeyword/FinalVariable.java
@@ -1,11 +1,20 @@
 package day53_finalKeyword;
 
+import java.time.LocalDate;
+
 public class FinalVariable {
 
     public static void main(String[] args) {
 
+        final LocalDate dateofbirth = LocalDate.of(1985,07,13);
 
+       // dateofbirth = LocalDate.of(1991,06,26);
 
+        System.out.println(dateofbirth);
 
+        final long ssn = 1234567;
+        //ssn = 456789;
+
+        System.out.println(ssn);
     }
 }

6472028049f5c1d176c77c5874dfbd635ed2aa54
hasanovemil <emil-2035@mail.ru>
2023-01-14 15:47:51 +0400

    hasanov emil123

--- src/Office_Hours/Practice_09_09_2020/Phone.java
+++ src/Office_Hours/Practice_09_09_2020/Phone.java
@@ -2,5 +2,33 @@ package Office_Hours.Practice_09_09_2020;
 
 public class Phone {
 
+    public String model;
+    public int price;
+    public String madein;
+
+    public static String brand;
+
+    public Phone(String model, int price, String madein) {
+        this.model = model;
+        this.price = price;
+        this.madein = madein;
+    }
+    static{
+        brand = "MadeinChina";
+    }
+
+    public void call(){
+        System.out.println("");
+    }
+
+
+
+
+
+
+
+
+
+
 
 }

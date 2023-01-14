a99d652b1ed77f8e2dd9ef4606d861c31b8ccabf
hasanovemil <emil-2035@mail.ru>
2023-01-14 16:12:28 +0400

    hasanov emil123

--- src/Office_Hours/Practice_09_09_2020/Phone.java
+++ src/Office_Hours/Practice_09_09_2020/Phone.java
@@ -2,33 +2,26 @@ package Office_Hours.Practice_09_09_2020;
 
 public class Phone {
 
-    public String model;
-    public int price;
-    public String madein;
 
     public static String brand;
+    public static String madeIn;
 
-    public Phone(String model, int price, String madein) {
+    public String model;
+    public double price;
+
+    public Phone(String model,double price){
         this.model = model;
         this.price = price;
-        this.madein = madein;
     }
-    static{
-        brand = "MadeinChina";
+    public void call(long phonenumber){
+        System.out.println("Phone is calling: " + phonenumber);
     }
-
-    public void call(){
-        System.out.println("");
+    public void text(long phonenumber) {
+        System.out.println("Phone is texting to: " + phonenumber);
     }
 
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public String toString() {
+        return "Brand: "+brand+"\nModel: "+model+"\nPrice; "+price+"\nMadein: "+madeIn;
+    }
 }

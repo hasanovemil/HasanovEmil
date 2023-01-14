59f2cc975e3dbd29c03f34057bd119fa0da61292
hasanovemil <emil-2035@mail.ru>
2023-01-14 20:45:06 +0400

    hasanov emil123

--- src/day53_finalKeyword/FinalVariable.java
+++ src/day53_finalKeyword/FinalVariable.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 
 public class FinalVariable {
 
+    final static String eyecolor = "brown";
+    final String bloodtype = "A";
+
     public static void main(String[] args) {
 
         final LocalDate dateofbirth = LocalDate.of(1985,07,13);
@@ -16,5 +19,21 @@ public class FinalVariable {
         //ssn = 456789;
 
         System.out.println(ssn);
+
+        final double PI = 3.14;
+         // PI = 2.14;
+        System.out.println(PI);
+
+        final char gender = 'M';
+        //gender = 'F';
+
+        final String birthplace = "Azerbaijan";
+        //birthplace = "USA";
+
+
+
+
+
+
     }
 }

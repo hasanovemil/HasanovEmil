1d3300097785fc12475ddca73de2130695ff0da5
hasanovemil <emil-2035@mail.ru>
2023-01-22 21:10:42 +0400

    hasanov emil123

---
+++ src/Office_Hours/Practice_09_15_2020/Quiz.java
@@ -0,0 +1,26 @@
+package Office_Hours.Practice_09_15_2020;
+
+class Data{
+
+    public void methodA(){
+        System.out.println("Method A");
+    }
+}
+class Test extends Data{
+
+public void methodB(){
+    System.out.println("Method B");
+}
+
+    public static void main(String[] args) {
+        Data obj = new Data();
+        obj.methodA();
+    }
+
+
+}
+
+public class Quiz {
+
+
+}

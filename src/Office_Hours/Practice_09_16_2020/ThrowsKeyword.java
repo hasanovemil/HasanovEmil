9279de4a5b1bc205531380cb952c2872219018af
hasanovemil <emil-2035@mail.ru>
2023-01-24 22:57:24 +0400

    hasanov emil123

---
+++ src/Office_Hours/Practice_09_16_2020/ThrowsKeyword.java
@@ -0,0 +1,19 @@
+package Office_Hours.Practice_09_16_2020;
+
+public class ThrowsKeyword {
+
+    public static void main(String[] args) {
+
+        ThrowsKeyword obj = new ThrowsKeyword();
+        // obj.methodA();
+
+    }
+
+    public void methodA() throws InterruptedException{
+        Thread.sleep(3000);
+    }
+
+
+
+}
+

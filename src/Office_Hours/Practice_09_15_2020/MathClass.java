0a1d7e736f0b5ca465c3384a014c0cb75a63f819
hasanovemil <emil-2035@mail.ru>
2023-01-24 13:27:24 +0400

    hasanov emil123

---
+++ src/Office_Hours/Practice_09_15_2020/MathClass.java
@@ -0,0 +1,19 @@
+package Office_Hours.Practice_09_15_2020;
+
+public class MathClass {
+
+    public static void main(String[] args) {
+
+        double r1 = Math.pow(10,3);
+
+        System.out.println(r1);
+
+        System.out.println(Math.pow(2,5));
+
+
+
+    }
+
+
+
+}
---
+++ src/Office_Hours/Practice_09_15_2020/Quiz2.java
@@ -0,0 +1,10 @@
+package Office_Hours.Practice_09_15_2020;
+
+public class Quiz2 {
+
+    int [] arr1 = { 1,1,2,3,4,4,5,5,5,6,7,8,9,10};
+
+    
+
+
+}

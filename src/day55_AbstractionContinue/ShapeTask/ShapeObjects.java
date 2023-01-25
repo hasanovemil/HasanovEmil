08f3f1d867365227b49afcb1e58efd22c31b2328
hasanovemil <emil-2035@mail.ru>
2023-01-25 17:12:32 +0400

    hasanov emil123

--- src/day55_AbstractionContinue/ShapeTask/ShapeObjects.java
+++ src/day55_AbstractionContinue/ShapeTask/ShapeObjects.java
@@ -2,5 +2,14 @@ package day55_AbstractionContinue.ShapeTask;
 
 public class ShapeObjects {
 
+    public static void main(String[] args) {
+
+      Circle circle1 = new Circle(2.5);
+        System.out.println(circle1);
+
+
+
+    }
+
 
 }

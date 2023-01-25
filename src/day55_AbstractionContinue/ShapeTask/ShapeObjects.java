0a2c737e920f37e7493719af086838ba8ba29337
hasanovemil <emil-2035@mail.ru>
2023-01-25 17:18:05 +0400

    hasanov emil123

--- src/day55_AbstractionContinue/ShapeTask/ShapeObjects.java
+++ src/day55_AbstractionContinue/ShapeTask/ShapeObjects.java
@@ -6,6 +6,17 @@ public class ShapeObjects {
 
       Circle circle1 = new Circle(2.5);
         System.out.println(circle1);
+        System.out.println("Circle has volume: "+circle1.hasVolume);
+
+        Circle circle2 = new Circle(5.5);
+        System.out.println(circle2);
+
+        Rectangle rectangle1 = new Rectangle(5,8);
+
+        System.out.println(rectangle1);
+
+        Cylinder cylinder1 = new Cylinder(5,10);
+        System.out.println(cylinder1);
 
 
 

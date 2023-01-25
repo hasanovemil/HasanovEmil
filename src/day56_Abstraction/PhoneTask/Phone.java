d8483b0baa72e91ecf5ee46ff2b7561b539ce9ea
hasanovemil <emil-2035@mail.ru>
2023-01-25 19:11:02 +0400

    hasanov emil123

--- src/day56_Abstraction/PhoneTask/Phone.java
+++ src/day56_Abstraction/PhoneTask/Phone.java
@@ -1,6 +1,31 @@
 package day56_Abstraction.PhoneTask;
 
-public class Phone {
+public abstract class Phone {
+
+    public String brand;
+    public String model;
+    public double price;
+    public String size;
+
+    public abstract void calling(long phonenumber);
+    public abstract void texting(long phonenumber);
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                ", size='" + size + '\'' +
+                '}';
+    }
+
+    public Phone(String brand, String model, double price, String size) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.size = size;
+    }
 
 
 }

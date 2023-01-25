1a7acb673ac9818a2454e99fe9a8e4bcae8beed0
hasanovemil <emil-2035@mail.ru>
2023-01-25 17:57:44 +0400

    hasanov emil123

---
+++ src/day55_AbstractionContinue/AbsClass_VS_regClass.java
@@ -0,0 +1,32 @@
+package day55_AbstractionContinue;
+
+
+
+class A{
+    int a = 10;
+    static int b = 20;
+    public void method1(){}
+    public static void method2(){}
+    static{}
+    public A(){}
+    // public abstract void method3();
+
+}
+
+abstract class B{
+    int a = 10;
+    static int b = 20;
+    public void method1(){}
+    public static void method2(){}
+    static{}
+    public B(){
+
+    }
+    public abstract void method3();
+
+}
+
+public class AbsClass_VS_regClass {
+
+
+}

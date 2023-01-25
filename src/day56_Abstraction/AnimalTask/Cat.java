5820dfd59315c0598b9a8d5ce807b3d94e2197fa
hasanovemil <emil-2035@mail.ru>
2023-01-25 22:53:17 +0400

    hasanov emil123

---
+++ src/day56_Abstraction/AnimalTask/Cat.java
@@ -0,0 +1,23 @@
+package day56_Abstraction.AnimalTask;
+
+public class Cat extends Animal implements Playable {
+
+
+    @Override
+    public void eat() {
+        System.out.println("Cat is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Cat is sleeping");
+    }
+
+
+    @Override
+    public void Play() {
+        System.out.println("cat is playing");
+    }
+
+
+}

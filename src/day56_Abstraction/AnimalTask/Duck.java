808a5e6361eaf3864c28ee640933b73c7f4bb59a
hasanovemil <emil-2035@mail.ru>
2023-01-25 23:26:27 +0400

    hasanov emil123

---
+++ src/day56_Abstraction/AnimalTask/Duck.java
@@ -0,0 +1,30 @@
+package day56_Abstraction.AnimalTask;
+
+public class Duck extends Animal implements Swimmable,Flyable,Playable {
+
+
+    @Override
+    public void eat() {
+        System.out.println("Duck is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Duck is sleeping");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Duck is swimming");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Duck is flying");
+    }
+
+    @Override
+    public void Play() {
+        System.out.println("Duck is playing");
+    }
+}

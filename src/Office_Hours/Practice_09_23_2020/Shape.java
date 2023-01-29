1b9598949c08124e4c89abe85f1ff8ac07e81107
hasanovemil <emil-2035@mail.ru>
2023-01-29 18:30:32 +0400

    hasanov emil123

--- src/Office_Hours/Practice_09_23_2020/Shape.java
+++ src/Office_Hours/Practice_09_23_2020/Shape.java
@@ -1,4 +1,14 @@
 package Office_Hours.Practice_09_23_2020;
 
-public class Shape {
+public abstract class Shape {
+
+    public double area;
+    public double perimeter;
+
+    public abstract double calculatearea();
+    public abstract double calculateperimeter();
+
+
+
+
 }
---
+++ src/Office_Hours/Practice_09_23_2020/Square.java
@@ -0,0 +1,15 @@
+package Office_Hours.Practice_09_23_2020;
+
+public class Square extends Shape {
+    @Override
+    public double calculatearea(){
+         return 0;
+    }
+
+    @Override
+    public double calculateperimeter() {
+        return 0;
+    }
+
+
+}

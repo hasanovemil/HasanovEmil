bda108fc1864eb7b0cc6594dde66b86223274a97
hasanovemil <emil-2035@mail.ru>
2023-02-01 23:28:51 +0400

    hasanov emil123

--- src/day58_Polymorphism/AnimalTask/Cat.java
+++ src/day58_Polymorphism/AnimalTask/Cat.java
@@ -5,7 +5,7 @@ import day58_Polymorphism.AnimalTask.Animal;
 public class Cat extends Animal {
     public String catName;
 
-    public Cat(int age, char gender, String catName) {
+    public Cat(String catName,int age, char gender ) {
         super(age, gender);
         this.catName = catName;
     }
--- src/day58_Polymorphism/InstanceOf_Keyword.java
+++ src/day58_Polymorphism/InstanceOf_Keyword.java
@@ -1,4 +1,30 @@
 package day58_Polymorphism;
 
+import day58_Polymorphism.AnimalTask.Animal;
+import day58_Polymorphism.AnimalTask.Cat;
+import day58_Polymorphism.AnimalTask.Dog;
+
 public class InstanceOf_Keyword {
+
+    public static void main(String[] args) {
+
+        Animal animal1 = new Animal(10,'F');
+        Animal animal2 = new Dog("Winston",4,'M');
+        Animal animal3 = new Cat("Kiko",2,'M');
+
+        boolean r1 = animal3 instanceof Animal;
+
+        System.out.println(r1);
+
+        boolean r2 = animal3 instanceof Cat;
+
+        System.out.println(r2);
+
+        boolean r3 = animal3 instanceof Dog;
+
+        System.out.println(r3);
+
+
+
+    }
 }

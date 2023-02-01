bcc5ab791236d56de25ea8d2376f909a057419b6
hasanovemil <emil-2035@mail.ru>
2023-02-02 02:00:41 +0400

    hasanov emil123

--- src/day58_Polymorphism/InstanceOf_Keyword.java
+++ src/day58_Polymorphism/InstanceOf_Keyword.java
@@ -1,12 +1,15 @@
 package day58_Polymorphism;
 
+
 import day58_Polymorphism.AnimalTask.Animal;
 import day58_Polymorphism.AnimalTask.Cat;
 import day58_Polymorphism.AnimalTask.Dog;
 
 public class InstanceOf_Keyword {
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args,Object shape) {
 
         Animal animal1 = new Animal(10,'F');
         Animal animal2 = new Dog("Winston",4,'M');
@@ -27,6 +30,8 @@ public class InstanceOf_Keyword {
         boolean r4 = animal2 instanceof Animal;
 
         System.out.println(r4);
+
+
 
 
     }

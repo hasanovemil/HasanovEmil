5ffe40e150b65ab8726d15e830db65486764c2a9
hasanovemil <emil-2035@mail.ru>
2023-02-01 13:06:19 +0400

    hasanov emil123

--- src/day57_Polymorphism/Samsung.java
+++ src/day57_Polymorphism/Samsung.java
@@ -1,23 +1,34 @@
 package day57_Polymorphism;
 
-public class Samsung extends Phone implements AndroidApp{
+public class Samsung extends Phone implements AndroidApp,Downloadable{
 
     public Samsung(String model, String size, double price) {
         super("Samsung", model, size, price);
+        if(price>1200){
+            System.out.println("Price of samsung cannot be greater than 1200");
+        }
     }
 
     @Override
     public void download() {
-
+        System.out.println("Samsung is downloading android app:"+AppstoreName);
     }
 
     @Override
     public void texting(long phonenumber) {
-
+        System.out.println("Samsung is texting: "+phonenumber);
     }
 
     @Override
     public void calling(long phonenumber) {
-
+        System.out.println("Samsung is calling: "+phonenumber);
     }
+
+    public void freezing(){
+        System.out.println("Samsung is freezing");
+    }
+
+
+
+
 }

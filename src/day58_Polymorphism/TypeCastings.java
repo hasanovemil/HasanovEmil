81cacf8ce883312351aa6b2e443f0e18d70aba72
hasanovemil <emil-2035@mail.ru>
2023-02-04 14:54:44 +0400

    hasanov emil123

--- src/day58_Polymorphism/TypeCastings.java
+++ src/day58_Polymorphism/TypeCastings.java
@@ -1,5 +1,8 @@
 package day58_Polymorphism;
 
+import day57_Polymorphism.PhoneTasks.Iphone;
+import day57_Polymorphism.PhoneTasks.Phone;
+import day57_Polymorphism.PhoneTasks.Samsung;
 import day58_Polymorphism.AnimalTask.Animal;
 import day58_Polymorphism.AnimalTask.Cat;
 import day58_Polymorphism.AnimalTask.Dog;
@@ -16,7 +19,28 @@ public class TypeCastings {
         System.out.println("===================================================");
 
         Animal animal3 = new Dog("A",2,'M');
+      //  animal3.bark();
+
         Dog dog2 = (Dog) animal3;
+        dog2.bark();
+        System.out.println("=================================================");
+
+        Iphone phone1 = new Iphone("12","6",1200);
+
+        Phone phone2 = phone1;
+
+        Phone phone3 = new Samsung("A53","8",1100);
+        Samsung phone4 = (Samsung) phone3;
+
+       // Iphone phone5 = (Iphone) phone3;
+
+
+
+
+
+
+
+
 
 
 

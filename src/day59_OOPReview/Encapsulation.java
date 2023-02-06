8785fb52417fb16f6c9d03a576a5fbe9ed251728
hasanovemil <emil-2035@mail.ru>
2023-02-06 21:35:03 +0400

    hasanov emil123

--- src/day59_OOPReview/Encapsulation.java
+++ src/day59_OOPReview/Encapsulation.java
@@ -1,4 +1,22 @@
 package day59_OOPReview;
 
+import java.time.LocalDate;
+
 public class Encapsulation {
+
+    private int ssn;
+
+    private final LocalDate Published = LocalDate.of(2020,9,3);
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public LocalDate getPublished() {
+        return Published;
+    }
 }

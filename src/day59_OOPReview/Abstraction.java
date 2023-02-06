b5b53661f698a359a84723062fd6cc4bbed29bfc
hasanovemil <emil-2035@mail.ru>
2023-02-06 23:44:37 +0400

    hasanov emil123

--- src/day59_OOPReview/Abstraction.java
+++ src/day59_OOPReview/Abstraction.java
@@ -1,4 +1,29 @@
 package day59_OOPReview;
 
+
+interface X{
+
+    public abstract void method1();
+
+}
+
+ abstract class Y {
+
+    public abstract void method1();
+          int a = 100;
+          static int b = 200;
+
+}
+
 public class Abstraction {
+    public static void main(String[] args) {
+       // X obj = new X();
+       // Y obj2 = new Y();
+
+
+    }
+
+
+
+
 }

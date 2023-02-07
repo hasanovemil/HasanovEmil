d2c184daf50e9ebc9a1b387f39bb6c2edff2c0e8
hasanovemil <emil-2035@mail.ru>
2023-02-07 20:10:25 +0400

    hasanov emil123

--- src/day60_Collection/ListInterface.java
+++ src/day60_Collection/ListInterface.java
@@ -53,6 +53,72 @@ public class ListInterface {
 
         System.out.println(list4.get(2));
 
+        ((Stack)list4).pop();
+
+        System.out.println(list4);
+
+        System.out.println("======================================================");
+
+        Stack<Integer> list5 = new Stack<>();
+
+        list5.addAll(Arrays.asList(1,2,3,4,5,6,7));
+
+        System.out.println(list5);
+
+        int lastObject = list5.pop();
+
+        System.out.println(list5);
+
+        System.out.println("=========================");
+
+        int[] arr1 = {1,2,3,4,5};
+        int[][] arr2 = {{1,2,3},{4,5,6,7,8,9}};
+
+        ArrayList<String> l1 = new ArrayList<>();
+        l1.add("A");
+
+
+
+        ArrayList<ArrayList<Integer>> l2 = new ArrayList<>();
+        l2.add(new ArrayList<>(Arrays.asList(1,2,3,4)));
+        l2.add(new ArrayList<>());
+
+        l2.get(1).addAll(Arrays.asList(5,6,7,8,9));
+
+        l2.get(0).removeAll(Arrays.asList(1,2));
+
+        System.out.println(l2);
+
+        System.out.println("Size of list1 = " + l2.get(0).size());
+
+        System.out.println("Size of list2 = " + l2.get(1).size());
+
+
+        List<List<String>> groups = new ArrayList<>();
+        groups.addAll(Arrays.asList(new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>(),
+                new ArrayList<>()));
+
+        groups.get(0).addAll(Arrays.asList("A","B"));
+        groups.get(1).addAll(Arrays.asList("C","D"));
+        groups.get(2).addAll(Arrays.asList("E","F"));
+        groups.get(3).addAll(Arrays.asList("M","N"));
+        groups.get(4).addAll(Arrays.asList("O","P"));
+        groups.get(5).addAll(Arrays.asList("X","Y"));
+        groups.get(6).addAll(Arrays.asList("T","Z"));
+
+        for(List<String> eachgroup: groups){
+            for(String eachstudent:eachgroup){
+                System.out.println(eachstudent +"  ");
+            }
+        }
+
+
+
 
 
     }

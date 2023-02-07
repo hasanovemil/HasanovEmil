8e3826394f45a83ee17157f45a33cbc29b6a3d87
hasanovemil <emil-2035@mail.ru>
2023-02-08 00:31:52 +0400

    hasanov emil123

--- src/day60_Collection/NullKeyword.java
+++ src/day60_Collection/NullKeyword.java
@@ -7,9 +7,9 @@ public class NullKeyword {
 
         System.out.println(s1.toUpperCase());
 
-        String s2 = null;
+      // String s2 = null;
 
-        System.out.println(s2.toUpperCase());
+      //  System.out.println(s2.toUpperCase());
 
 
 

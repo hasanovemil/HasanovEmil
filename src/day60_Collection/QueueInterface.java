5f2195f2bc329fb22b55352df39a9336e806b01d
hasanovemil <emil-2035@mail.ru>
2023-02-07 21:30:45 +0400

    hasanov emil123

--- src/day60_Collection/QueueInterface.java
+++ src/day60_Collection/QueueInterface.java
@@ -1,7 +1,30 @@
 package day60_Collection;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+
 public class QueueInterface {
 
+    public static void main(String[] args) {
+
+        Queue<Integer> q1 = new ArrayDeque<>();
+
+        q1.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        System.out.println(q1);
+        int firstobject =  q1.poll();
+
+        System.out.println(q1);
+
+        int secondobject = q1.poll();
+        System.out.println(q1);
+
+
+
+
+    }
 
 
 }
---
+++ src/day60_Collection/SetInterface.java
@@ -0,0 +1,29 @@
+package day60_Collection;
+
+
+import java.util.*;
+
+public class SetInterface {
+
+    public static void main(String[] args) {
+
+        Set<Integer> s1 = new HashSet<>();
+
+        Set<Integer> s2 = new LinkedHashSet<>();
+
+        Set<Integer> s3 = new TreeSet<>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

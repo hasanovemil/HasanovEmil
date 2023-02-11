f3277e298b4857934cece55dd662861698aebb73
hasanovemil <emil-2035@mail.ru>
2023-02-11 23:39:15 +0400

    hasanov emil123

--- src/day61_Maps/MapPractice2.java
+++ src/day61_Maps/MapPractice2.java
@@ -1,6 +1,8 @@
 package day61_Maps;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 
 public class MapPractice2 {
@@ -11,15 +13,37 @@ public class MapPractice2 {
         students.put("Rashad",95);
         students.put("Gunel",85);
         students.put("Ali",98);
-        students.put("Arif",75);
+        students.put("Vuqar",92);
+        students.put("Aqil",95);
+        students.put("Teymur",95);
+        students.put("Seymur",93);
+        students.put("Xeyale",100);
+        students.put("Vasif",71);
+        students.put("Valid",50);
+        students.put("Nesimi",60);
 
-        for(String eachKey :students.keySet()){
+
+      //  LinkedHashMap<String,Integer> earlyBirds = new LinkedHashMap<>();
+       // LinkedHashMap<String,Integer> angryBirds = new LinkedHashMap<>();
+
+        List<String> earlyBirdsList = new ArrayList<>();
+
+
+        for(String eachKey :students.keySet()) {
             Integer eachvalue = students.get(eachKey);
-            if(eachvalue < 80){
-                System.out.println(eachKey);
+
+            /* if(eachvalue >= 95){
+                earlyBirds.put(eachKey,eachvalue);
+            }else{
+                angryBirds.put(eachKey,eachvalue);
             }
         }
-
+        */
+            if (eachvalue > 90) {
+                earlyBirdsList.add(eachKey);
+            }
+        }
+        System.out.println(earlyBirdsList);
 
 
 
---
+++ src/day61_Maps/MapPractice3.java
@@ -0,0 +1,27 @@
+package day61_Maps;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapPractice3 {
+    public static void main(String[] args) {
+
+        Map<String,String> countries = new LinkedHashMap<>();
+        countries.put("East Turkistan","Kashgar");
+        countries.put("Uzbekistan","Tashkent");
+        countries.put("Kyrgyzstan","Bishkek");
+        countries.put("Spain","Madrid");
+        countries.put("USA","Washington");
+        countries.put("Ukraine","Kiev");
+        countries.put("Azrebaijan","Baku");
+        countries.put("Turkey","Ankara");
+
+
+
+
+
+
+
+
+    }
+}

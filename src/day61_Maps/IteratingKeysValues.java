2723b8d62cf9ddec119ad36ec067458e20b11191
hasanovemil <emil-2035@mail.ru>
2023-02-11 23:02:11 +0400

    hasanov emil123

--- src/day61_Maps/IteratingKeysValues.java
+++ src/day61_Maps/IteratingKeysValues.java
@@ -1,5 +1,35 @@
 package day61_Maps;
 
-public class IteratingKeysValues {
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
+public class IteratingKeysValues {
+    public static void main(String[] args) {
+
+        Map<String, LocalDate> students = new TreeMap<>();
+
+        students.put("Hasan", LocalDate.of(1985,4,4));
+        students.put("Ali",LocalDate.of(1979,1,5));
+        students.put("Arif",LocalDate.of(1976,5,16));
+
+        System.out.println(students);
+
+        for(String each : students.keySet()){
+            System.out.println(each);
+        }
+
+        for(LocalDate each: students.values()){
+            System.out.println(each);
+        }
+
+
+
+
+
+
+
+
+    }
 }

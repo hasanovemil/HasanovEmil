79beee81cec44f0fd71c73279af66f5db2c0a55b
hasanovemil <emil-2035@mail.ru>
2023-02-11 20:27:43 +0400

    hasanov emil123

---
+++ src/day61_Map/RemovePalindrome.java
@@ -0,0 +1,22 @@
+package day61_Map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemovePalindrome {
+
+    public static void main(String[] args) {
+
+        String[] str = {"Emil","Rashad","level","Madam"};
+
+        List<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList(str));
+
+
+
+
+
+
+    }
+}

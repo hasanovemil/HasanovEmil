a0d7d1d5af16ec93bf6c1162887e920702324661
hasanovemil <emil-2035@mail.ru>
2023-02-14 01:22:40 +0400

    hasanov emil123

--- src/day62_Maps/Frequencyofchars.java
+++ src/day62_Maps/Frequencyofchars.java
@@ -1,7 +1,27 @@
 package day62_Maps;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Frequencyofchars {
     public static void main(String[] args) {
+
+        String str = "aaabbbccb";   //a2b4c2
+
+        Map<String,Integer> map = new LinkedHashMap<>();  // { a = 3, b = 4, c = 2 }
+
+        List<String> list = Arrays.asList(str.split(""));
+        System.out.println(list);
+
+        for(String each : list){
+
+
+
+        }
+
+
 
 
 

fed0f8074a95abfcd72e7bb43f46e9ddba14fb71
hasanovemil <emil-2035@mail.ru>
2023-02-14 21:20:29 +0400

    hasanov emil123

--- src/day62_Maps/Test.java
+++ src/day62_Maps/Test.java
@@ -9,6 +9,35 @@ public class Test {
 
         System.out.println(favoritecolor);
 
+        Browser browsername = Browser.chrome;
+
+        switch (browsername){
+            case chrome:
+                System.out.println("Chrome browser is set");
+                break;
+
+            case firefox:
+                System.out.println("Firefox browser is set");
+                break;
+
+            case safari:
+                System.out.println("Safari browser is set");
+                break;
+
+            case edge:
+                System.out.println("Edge browser is set");
+                break;
+
+
+
+
+
+
+
+        }
+
+
+
 
 
 

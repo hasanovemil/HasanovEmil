ee067fb190c426770aa38eceeabbd8f20d727309
hasanovemil <emil-2035@mail.ru>
2023-02-14 20:54:43 +0400

    hasanov emil123

--- src/day62_Maps/GarbageCollectionReview.java
+++ src/day62_Maps/GarbageCollectionReview.java
@@ -1,5 +1,8 @@
 package day62_Maps;
 
+import day55_Abstraction.ShapeTask.Circle;
+import day55_Abstraction.ShapeTask.Rectangle;
+
 public class GarbageCollectionReview {
     public static void main(String[] args) {
 
@@ -14,6 +17,39 @@ public class GarbageCollectionReview {
         s1 = s2;
 
         System.out.println(s1 + " " + s2);
+
+        Circle c1 = new Circle(3);
+        c1 = null;
+
+        System.out.println(c1);
+
+        Rectangle r1 = new Rectangle(10,20);
+        Rectangle r2 = new Rectangle(2,5);
+
+         r1 = r2;
+        System.out.println(r1);
+        System.out.println(r2);
+
+        System.out.println("==============================================");
+
+        String a1 = "Cybertek";
+        a1 = null;
+
+        System.out.println(a1);
+
+        String a2 = "MIT";
+        String a3 = "Harvard";
+        a2 = a3;
+
+        System.out.println(a2);
+        System.out.println(a3);
+
+        System.out.println("=============================================");
+
+
+
+
+
 
 
 
---
+++ src/day62_Maps/Test.java
@@ -0,0 +1,18 @@
+package day62_Maps;
+
+public class Test {
+    public static void main(String[] args) {
+
+      //  String favoritecolor = "Blue";
+
+        Color favoritecolor = Color.Blue;
+
+        System.out.println(favoritecolor);
+
+
+
+
+
+
+    }
+}
